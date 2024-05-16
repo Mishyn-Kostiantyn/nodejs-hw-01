e9f3d03c88ac175d108d0322edd737c5e72f5595
Mishyn-Kostiantyn <129659279+Mishyn-Kostiantyn@users.noreply.github.com>
2024-05-17 00:58:52 +0300

    Ще змінив повідомлення

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -4,6 +4,7 @@ import fs from 'node:fs/promises';
 export const getAllContacts = async () => {     let exsistingContacts = [];
     try {
         exsistingContacts = JSON.parse(await fs.readFile(PATH_DB));
+        if (exsistingContacts.length === 0) { console.log('Немає контактів для відображення!'); return; } 
         
      } catch (error) { console.log('Помилка зчитування існуючих контактів', error); }
    return exsistingContacts;};
