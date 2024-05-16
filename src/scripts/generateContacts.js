854b78d4c43be3b2b83fc043934ed06658c6e86b
Mishyn-Kostiantyn <129659279+Mishyn-Kostiantyn@users.noreply.github.com>
2024-05-17 00:40:56 +0300

    Модуль 1. Виконав ДЗ

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,20 @@
-import { PATH_DB } from '../constants/contacts.js';
+import { PATH_DB} from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
 
-await generateContacts(5);
+const generateContacts = async (number) => {
+    let oldContacts = [];
+    try { oldContacts = JSON.parse(await fs.readFile(PATH_DB)); } catch (error) { console.log('Помилка зчитування існуючих контактів', error); }
+    let newContacts=[];
+    for (let i = 0; i < number; i += 1) {
+         newContacts.push(createFakeContact());
+    };
+    let allContacts=oldContacts.concat(newContacts);
+    try { await fs.writeFile(PATH_DB, JSON.stringify(allContacts)); console.log('Контакти записано!'); } catch (error) { console.log('Помилка запису контактів', error); }
+        finally {
+        console.log("Загальна кількість контактів:", allContacts.length);
+    }
+};
+
+await generateContacts(3);
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {     let exsistingContacts = [];
+    try {
+        exsistingContacts = JSON.parse(await fs.readFile(PATH_DB));
+        
+     } catch (error) { console.log('Помилка зчитування існуючих контактів', error); }
+   return exsistingContacts;};
+
 
 console.log(await getAllContacts());
